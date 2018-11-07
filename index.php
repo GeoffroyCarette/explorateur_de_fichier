f57e210e6e4020bf1e96cb9455c7f553f7957506
Lucas Robin <lucas.rob1@live.fr>
2018-11-07 11:10:58 +0100

    Système de navigation fonctionnel
    - Suivant et précédent fonctionnel 
    - TODO: array URL à nettoyer

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 require_once './vendor/autoload.php';
 $liste_fichier = array();
+// session_start();
+// $_SESSION['newsession'] = $array_chemin;
+$chemins_stock = array();
 
 // Rendu du template
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/php/templates');
@@ -12,22 +15,19 @@ $twig = new Twig_Environment($loader, [
 
 // Routing
 if (isset($_GET["fichier"])) { // Si la var existe
+		$chemins_stock[] = $_GET["fichier"];
 		if($mainDir = opendir($_GET["fichier"])) { // Si le répertoire existe
 			while(false !== ($fichier = readdir($mainDir))) { // fait la boucle tant qu'il n'y a pas d'erreurs
-				if($fichier != '..' && $fichier != 'index.php') { // gère les exceptions
+				if($fichier != '.' && $fichier != 'index.php') { // gère les exceptions
 					$liste_fichier[] = $fichier; // ajoute chaque itération au tableau
-				}
+				} else if ($fichier == 'index.php');
 			}
         };
         // appelle grille.twig et lui passe le tableau
 		echo $twig->render('grille.twig', array('liste_fichier' => $liste_fichier));
-	
 } else {
 	echo $twig->render("index.twig"); // appelle l'index par défaut
 }
-
-	
-
 
 
 ?>
