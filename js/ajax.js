32abadc76af6de9d929ca7ffb7447168804ba670
Lucas Robin <lucas.rob1@live.fr>
2018-11-07 13:47:31 +0100

    avant fusion du front et du back

--- js/ajax.js
+++ js/ajax.js
@@ -8,7 +8,6 @@ window.onload = () => {
             grille = document.querySelector("#grille");
             grille.innerHTML = response;
             files = document.querySelectorAll(".fichier");
-
         })
         .catch((error) => { console.log(error) })
 }
@@ -17,6 +16,11 @@ window.onload = () => {
 let url_array = ["."];
 window.addEventListener("click", (event) => {
     url_array.push(event.target.getAttribute("data-path"));
+    if (event.target.getAttribute("data-path") == ".." && url_array.length > 1) {
+        url_array.splice(url_array.length - 2, url_array.length);
+    } else if (event.target.getAttribute("data-path") == ".." && url_array.length == 1) {
+		url_array.pop();
+	}
     if (event.target.classList.contains("fichier")) {
         console.log(event.target.getAttribute("data-path"));
         fetch(`/GitHub/ACS/explorateur_de_fichier/index.php?fichier=${arrayToUrl(url_array)}`)
@@ -33,6 +37,3 @@ function arrayToUrl(array) {
     return array.toString().split(",").join("/");
 }
 
-function cleanUrl(url) {
-
-}
