cb373aea25c38c5324fddbd819c42c261881ce0c
Unknown <jules.p@codeur.online>
2018-11-09 08:56:59 +0100

    Branche

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $twig = new Twig_Environment($loader, [
 if (isset($_GET["fichier"])) { // Si la var existe
 		if (is_dir($_GET["fichier"])) {
 		$chemin = realpath($_GET["fichier"]); // On assigne à une variable le chemin de la variable GET
-
+			$isFile = false;
 		$mainDir = opendir($chemin); // On ouvre le dossier/fichier via son chemin
 		if ($mainDir) { // Si le répertoire existe
 			while(false !== ($fichier = readdir($mainDir))) { // fait la boucle tant qu'il n'y a pas d'erreurs
@@ -29,9 +29,10 @@ if (isset($_GET["fichier"])) { // Si la var existe
 	}
 	else {
 		$file = file_get_contents($_GET["fichier"]);
+		$isFile = true;
 	}
         // appelle grille.twig et lui passe le tableau
-		$grille = $twig->render('grille.twig', array('liste_fichier' => $liste_fichier));
+		$grille = $twig->render('grille.twig', array('liste_fichier' => $liste_fichier, 'file' => $file, 'isFile' => $isFile));
 		$nav =  $twig->render('nav.twig', array('chemin' => $chemin));
         $sendToJS = array('grille' => $grille, 'chemin' => $nav);
         echo json_encode($sendToJS);
