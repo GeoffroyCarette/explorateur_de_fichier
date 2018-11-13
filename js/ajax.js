f0c5825987149a4d0c72cb0a1dc405c2afb414d2
Geoffroy Carette <geoffroy.c@codeur.online>
2018-11-13 14:36:40 +0100

    Version final

--- js/ajax.js
+++ js/ajax.js
@@ -14,15 +14,17 @@ window.addEventListener("mouseover", (event) => {
     if (event.target.classList.contains("fichier")) {
         if (event.target.getAttribute("data-path").split(".").length > 1) {
             event.target.style.cursor = "not-allowed";
+            // event.target.style.pointerEvents = "none";
+
         }
     }
 })
 
 // Si on double click sur un icon, fais un fetch
 window.addEventListener("dblclick", (event) => {
-    transitionOpacity(0.0);
     // S'assure que l'on ne puisse pas double cliquer sur les boutons de navigation et l'arborescence
-    if (containsClass("fichier")) {
+    if (containsClass("fichier") && event.target.getAttribute("data-path").split(".").length <= 1) {
+        transitionOpacity(0.0);
         // Ajoute le dossier au chemin
         isDir(event.target.getAttribute("data-path"), false);
         // Render
@@ -61,11 +63,11 @@ window.addEventListener("click", (event) => {
 
     let allFichier = document.querySelectorAll('.fichier');
 
-    for (i=0; i<allFichier.length; i++) {
+    for (i = 0; i < allFichier.length; i++) {
         allFichier[i].parentNode.classList.remove("backgroundParent");
     }
-    if (event.target.classList.contains("fichier")){
-        event.target.parentNode.classList.add("backgroundParent"); 
+    if (event.target.classList.contains("fichier")) {
+        event.target.parentNode.classList.add("backgroundParent");
     }
     // Si on clique sur un élément à gauche , fais un fetch
     if (containsClass("aside-elem")) {
